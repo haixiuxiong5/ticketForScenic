45a07ad8ee86463eeae835ba95ba0694cb35dca0
xujuan <xujuan@liujiedeMacBook-Pro.local>
2019-07-30 22:07:03 +0800

    commit

--- src/layout/home.vue
+++ src/layout/home.vue
@@ -1,29 +1,55 @@
 <template>
   <div class="home">
-    <Header></Header>
+    <!-- <Header></Header> -->
+    <van-nav-bar
+      title="标题"
+      :left-arrow="backShow"
+      @click-left="onClickLeft"
+    />
     <div class="container">
       <router-view></router-view>
     </div>
   </div>
 </template>
 <script>
-import Header from "./header";
+// import Header from "./header";
+import { NavBar } from "vant";
 export default {
   data() {
-    return {};
+    return {
+      backShow: false
+    };
   },
   components: {
-    Header
+    // Header
+    [NavBar.name]: NavBar
+  },
+  created() {
+    this.backShow = this.$route.path === "/" ? false : true
+  },
+  watch: {
+    $route(now, old) {
+      if (now.path == "/") {
+        this.backShow = false;
+      } else {
+        this.backShow = true;
+      }
+    }
+  },
+  methods: {
+    onClickLeft() {
+      this.$router.go(-1)
+    }
   }
 };
 </script>
 <style>
 .home {
-  padding-top: 44px;
+  /* padding-top: 44px; */
 }
 .container {
   position: fixed;
-  top: 44px;
+  top: 46px;
   bottom: 0;
   left: 0;
   right: 0;
